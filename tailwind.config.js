573bb77933f7a0ba45815334666fdbfc5a8bb4d2
Felipe Manchester <f.p2809@gmail.com>
2026-01-31 18:21:02 -0300

    Enhance styling and functionality: add new utility classes for animations, update font families, and improve layout in index.html and CSS files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin';
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
@@ -8,6 +10,10 @@ module.exports = {
         DEFAULT: '.5rem',
         sm: '1rem',
       },
+    },
+    fontFamily: {
+      sans: ['DM Sans', 'sytem-ui', 'sans-serif'],
+      serif: ['DM Serif Text', 'Georgia', 'serif'],
     },
     extend: {
       colors: {
@@ -24,14 +30,49 @@ module.exports = {
 
       animation: {
         'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        ['slide-in']: 'slideIn .4s ease-in-out forwards ',
+        ['fade-in']: 'fadeIn .4s ease-in-out forwards ',
       },
       keyframes: {
         'infinite-scroll': {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         },
+        slideIn: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateX(-20px)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateX(0)',
+          },
+        },
+        fadeIn: {
+          '0%': {
+            opacity: 0,
+          },
+          '100%': {
+            opacity: 1,
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      function animationDelay() {
+        const delays = {};
+
+        for (let i = 0; i <= 12; i++) {
+          delays[`.animate-${i}`] = {
+            'animation-delay': `${i * 100}ms`,
+          };
+        }
+        return delays;
+      }
+
+      addUtilities(animationDelay());
+    }),
+  ],
 };
